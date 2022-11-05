6c54967294291f08f644876306193e23d36e8eae
Chris obrien <chrisobrien88@gmail.com>
2022-11-05 13:10:47 +0100

    css changes

--- index.js
+++ index.js
@@ -1,80 +1,4 @@
-// const hexToComplimentary = require('./colorChanger')
 
-
-function hexToComplimentary(hex){
-
-    // Convert hex to rgb
-    // Credit to Denis http://stackoverflow.com/a/36253499/4939630
-    var rgb = 'rgb(' + (hex = hex.replace('#', '')).match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16); }).join(',') + ')';
-
-    // Get array of RGB values
-    rgb = rgb.replace(/[^\d,]/g, '').split(',');
-
-    var r = rgb[0], g = rgb[1], b = rgb[2];
-
-    // Convert RGB to HSL
-    // Adapted from answer by 0x000f http://stackoverflow.com/a/34946092/4939630
-    r /= 255.0;
-    g /= 255.0;
-    b /= 255.0;
-    var max = Math.max(r, g, b);
-    var min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2.0;
-
-    if(max == min) {
-        h = s = 0;  //achromatic
-    } else {
-        var d = max - min;
-        s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
-
-        if(max == r && g >= b) {
-            h = 1.0472 * (g - b) / d ;
-        } else if(max == r && g < b) {
-            h = 1.0472 * (g - b) / d + 6.2832;
-        } else if(max == g) {
-            h = 1.0472 * (b - r) / d + 2.0944;
-        } else if(max == b) {
-            h = 1.0472 * (r - g) / d + 4.1888;
-        }
-    }
-
-    h = h / 6.2832 * 360.0 + 0;
-
-    // Shift hue to opposite side of wheel and convert to [0-1] value
-    h+= 180;
-    if (h > 360) { h -= 360; }
-    h /= 360;
-
-    // Convert h s and l values into r g and b values
-    // Adapted from answer by Mohsen http://stackoverflow.com/a/9493060/4939630
-    if(s === 0){
-        r = g = b = l; // achromatic
-    } else {
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        };
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-
-    r = Math.round(r * 255);
-    g = Math.round(g * 255); 
-    b = Math.round(b * 255);
-
-    // Convert r b and g values to hex
-    rgb = b | (g << 8) | (r << 16); 
-    return "#" + (0x1000000 | rgb).toString(16).substring(1);
-}  
 
 const state = JSON.parse(localStorage.getItem('todos')) || [];
 
@@ -99,7 +23,7 @@ const todoListComplete = document.getElementById('todoListComplete')
 addBtn.addEventListener('click', () => addTodo());
 colorInput.addEventListener('click', () => console.log(colorInput.value));
 
-const checkBrightness = (inputColor, fontColor) => {
+const checkBrightness = (inputColor) => {
     const hex = inputColor.replace('#', '');
     const c_r = parseInt(hex.substr(0, 2), 16);
     const c_g = parseInt(hex.substr(2, 2), 16);
@@ -111,13 +35,10 @@ const checkBrightness = (inputColor, fontColor) => {
     }
     return 'white'
 }
-
-
 const addTodo = () => {
     try{
         inputValidator(todoInput.value);
-        checkBrightness(colorInput.value)
-        categoriesArr = state.map(todo => todo.category)
+        checkBrightness(colorInput.value);
 
         const todo = {
             id: Date.now(),
@@ -125,7 +46,6 @@ const addTodo = () => {
             category: todoCategoryInput.value,
             important: false,
             complete: 'not done',
-            randomNumber: Math.floor(Math.random() * 4),
             backgroundColor: colorInput.value,
             color: checkBrightness(colorInput.value),
         }
@@ -133,13 +53,10 @@ const addTodo = () => {
         state.find(oldTodo => {
             if (oldTodo.category === todo.category && todo.category !== '')
                 {
-                    todo.randomNumber = oldTodo.randomNumber;
                     todo.backgroundColor = oldTodo.backgroundColor;
                     todo.color = oldTodo.color;
-                    // todo.id = oldTodo.id + 1;
                 };
         });
-        console.log(todo);
         state.push(todo);
         todoInput.value = '';
         todoCategoryInput.value = ''}
@@ -190,7 +107,7 @@ const render = () => {
     ;
 }
 
-const createTodo = ({id, text, category, complete, important, randomNumber, backgroundColor, color}) => {
+const createTodo = ({id, text, category, complete, important, backgroundColor, color}) => {
     const deleteButton = `<button class='button deleteButton' onclick="deleteTodo(${id})">X</button>`
     const copyTextButton = `<button class="button copyButton" onclick="event.stopPropagation(); copyContent(${id})">Copy</button>`
     const importantButton = `
@@ -202,7 +119,6 @@ const createTodo = ({id, text, category, complete, important, randomNumber, back
                 ${complete === 'in progress' ? 'in-Progress': 'todo' }
                 ${complete === 'done' ? 'done' : 'todo'}
                 ${important? 'important' : 'not-important' }
-                ${complete !== 'done' ? randomNumber : ''}  
             ' 
             id=${id} 
             onclick="doneToggle(${id})">
